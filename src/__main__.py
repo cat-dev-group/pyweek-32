6bb97a4a3271dd14fb43f743b8821e782548614a
bradtimmis <bradleytimmis@gmail.com>
2021-09-23 20:06:51 -0400

    chore: make screen size 1000x1000

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,9 @@
 import arcade
-from dotenv import load_dotenv
 
 from .SCREAMING_SNAKE_SHOOTER import StartView
 
-load_dotenv("./.env")
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 1200
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
 SCREEN_TITLE = "SCREAMING_SNAKE_SHOOTER"
 
 if __name__ == "__main__":
