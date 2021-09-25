d969b6fe4d49f03cad6ac4d12c107f6cd100a7e3
bradtimmis <bradleytimmis@gmail.com>
2021-09-25 09:45:48 -0400

    chore: update text on button click

--- src/views/VendorView.py
+++ src/views/VendorView.py
@@ -20,6 +20,8 @@ class VendorScreen(arcade.View):
         super().__init__()
         self.pause_view = pause_view
         self.score = score
+        self.selected_text = ""
+        self.selected_gun = "default"
 
         # start gui manager to handle buttons
         self.manager = arcade.gui.UIManager()
@@ -27,35 +29,28 @@ class VendorScreen(arcade.View):
 
         # code for adding clickable buttons for guns
         self.h_box = arcade.gui.UIBoxLayout(vertical=False)
-        self.selected_text = ""
-        self.selected_gun = "default"
+
+        # create gun text
+        gun1_text = "PY90"
+        gun2_text = "STDLIB47"
+        gun3_text = "EX-47"
 
         # create gun buttons
-        gun1_text = "PY90"
-        gun1 = arcade.gui.UIFlatButton(text=gun1_text, width=100)
+        gun1 = arcade.gui.UIFlatButton(text=gun1_text, width=230)
+        gun2 = arcade.gui.UIFlatButton(text=gun2_text, width=230)
+        gun3 = arcade.gui.UIFlatButton(text=gun3_text, width=230)
+
+        # add buttons
         self.h_box.add(gun1.with_space_around(top=5, right=5, bottom=5, left=5))
-        gun2_text = "STDLIB-15"
-        gun2 = arcade.gui.UIFlatButton(text=gun2_text, width=125)
         self.h_box.add(gun2.with_space_around(top=5, right=5, bottom=5, left=5))
-        gun3_text = "EX-47"
-        gun3 = arcade.gui.UIFlatButton(text=gun3_text, width=100)
         self.h_box.add(gun3.with_space_around(top=5, right=5, bottom=5, left=5))
 
-        # Handle Clicks on gun buttons
+        # handle clicking on buttons
         @gun1.event("on_click")
-        def on_click_flatbutton(event):
-            self.selected_text = gun1_text
-            self.on_draw()
-
         @gun2.event("on_click")
-        def on_click_flatbutton(event):  # noqa F811: doesn't work when re-defined
-            self.selected_text = gun2_text
-            self.on_draw()
-
         @gun3.event("on_click")
-        def on_click_flatbutton(event):  # noqa F811: doesn't work when re-defined
-            self.selected_text = gun3_text
-            self.on_draw()
+        def on_click_gun(event):
+            self.selected_text = event.source.text
 
         # setup code for clickable `git push` button
         git_style = {
@@ -70,13 +65,16 @@ class VendorScreen(arcade.View):
             "anchor_x": "left",
             "anchor_y": "center",
         }
+
         # create new layout for widget
-        self.selection_box = arcade.gui.UIBoxLayout(vertical=False)
+        self.selection_box = arcade.gui.UIBoxLayout(vertical=True)
+
         # create button
         push_to_main_text = "$ git push -f origin main"
         push_to_main = arcade.gui.UIFlatButton(
             text=push_to_main_text, width=230, height=30, style=git_style
         )
+
         # add button to widget layout
         self.selection_box.add(push_to_main.with_space_around(0, 0, 0, 0))
 
@@ -84,10 +82,9 @@ class VendorScreen(arcade.View):
         # selected_text is defined by clicking on the boxes
         # TODO need to return this variable back to the main game window
         @push_to_main.event("on_click")
-        def on_click_flatbutton(event):  # noqa F811: doesn't work when re-defined
+        def on_click_push_to_main(event):  # noqa F811: doesn't work when re-defined
             self.selected_gun = self.selected_text
-            print(self.selected_gun)
-            self.on_draw()
+            print(f"{self.selected_gun=},{event=}")
 
         # add layouts to GUI manager
         self.manager.add(
@@ -101,6 +98,7 @@ class VendorScreen(arcade.View):
             arcade.gui.UIAnchorWidget(
                 child=self.selection_box,
                 anchor_x="left",
+                anchor_y="center",
                 align_x=SCREEN_WIDTH / 2 - 150,
                 align_y=SCREEN_HEIGHT / 2 - 290,
             )
@@ -166,7 +164,8 @@ class VendorScreen(arcade.View):
         )
 
         # 'terminal' commands
-        purchase_text = f"$ git add {self.selected_text:10}"
+        purchase_text = f"$ git add {self.selected_text:15}"
+        print(f"{purchase_text = }")
         arcade.draw_text(
             purchase_text,
             SCREEN_WIDTH / 2 - 148,
@@ -193,3 +192,5 @@ class VendorScreen(arcade.View):
         """Return to Pause screen on 'P' press."""
         if symbol == arcade.key.P:
             self.window.show_view(self.pause_view)
+        if symbol == arcade.key.ESCAPE:
+            arcade.close_window()
